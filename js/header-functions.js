242de90e7f8ead0ee7d699dbb3c1d066a0a3370a
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-07-09 14:47:21 -0400

    added a registration page (register.html) still very boilerplate from Bootstrap website, important to make the pages responsive

--- js/header-functions.js
+++ js/header-functions.js
@@ -1,3 +1,13 @@
+function writeFooterHTML() {
+  document.write(`
+    <div class="gn-footer">
+      This a beautiful footer. Extends farther probably 
+    </div>
+  
+  `);
+
+}
+
 function writeNavbarHTML() {
 
   document.write(`
@@ -11,10 +21,10 @@ function writeNavbarHTML() {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="./index.html">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Register</a>
+            <a class="nav-link" href="./register.html">Register</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
